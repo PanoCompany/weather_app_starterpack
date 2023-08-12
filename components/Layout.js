37776f1be562dfc061c556a2c1b1642185a5462e
ngocsnake <ducngoc.dev@gmail.com>
2023-08-12 20:07:39 +0700

    Update code

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <div className="w-[920px] h-[630px] bg-[#F6F6F8] rounded-3xl shadow flex">
       <TodayInfo />
+      
       <div className="p-8 grid grid-rows-3 w-full gap-8">
         <WeekInfo />
         <TodayHighlights />
--- components/TodayInfo.js
+++ components/TodayInfo.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function TodayInfo() {
   return (
-    <div className="w-[270px] bg-gray-400 p-8 h-full rounded-l-[inherit]"></div>
+    <div className="w-[300px] bg-gray-400 p-8 h-full rounded-l-[inherit]"></div>
   );
 }
